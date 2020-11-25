15bb3fed3ebd6858df427a2834cb7dfa4de96349
shaikzeeshan88 <shaikzeeshan88@gmail.com>
2020-11-25 11:26:34 +0530

    Given input validation to allow only for numbers

--- Basic Programs/Flipcoin.js
+++ Basic Programs/Flipcoin.js
@@ -1,20 +1,19 @@
 
-function flipCoin(noOfTimesToFlip){
-    let randomValue = 0;
+function flipCoin(){
     let heads = 0;
-    let tails = 0;
-//Prompt -- if you want the user to input a value before entering a page.
-//const noOfTimesToFlip = prompt('Please enter the number of times to Flip Coin');
-//const noOfTimesToFlip = document.getElementById("input").value;
-for(var index = 0; index < noOfTimesToFlip; index++){
-    // randomValue = Math.random();
-    //  if(randomValue > 0.5)
-    //     heads++;
-    //  else
-    //     tails++; 
-    (Math.random() > 0.5) ? (heads++) : (tails++);    // -- using ternary operator, alternative code for above code.             
+    let tails = 0;  
+    const noOfTimesToFlip = document.getElementById('input').value;             
+    for(var index = 0; index < noOfTimesToFlip; index++){ 
+        (Math.random() > 0.5) ? (heads++) : (tails++);    // -- using ternary operator, alternative code for above code.             
+    }
+    document.getElementById("Giveninput").innerHTML = noOfTimesToFlip; 
+    document.getElementById("heads").innerHTML = 'Number of heads ' + heads;
+    document.getElementById("tails").innerHTML = 'Number of tails ' + tails;
 }
-document.getElementById("Giveninput").innerHTML = noOfTimesToFlip; 
-document.getElementById("heads").innerHTML = 'Number of heads ' + heads;
-document.getElementById("tails").innerHTML = 'Number of tails ' + tails;
+
+const inputValidator = (event) => {
+    if(!Number(event.key)) {
+        alert('Please enter a number');
+        event.preventDefault();
+    }
 }
--- Basic Programs/Harmonic.js
+++ Basic Programs/Harmonic.js
@@ -1,13 +1,21 @@
 function HarmonicValue(valueOfN){
     let result = getHarmonicValue(valueOfN);
     document.getElementById('input').innerHTML = valueOfN;
-    document.getElementById('output').innerHTML = result;
-    function getHarmonicValue(valueOfN){
-        let result = 0;
-        for(var index = 1; index <= valueOfN; index++){
-            result += 1/index;
-        }
-        return result;
+    document.getElementById('output').innerHTML = result;    
+}
+
+function getHarmonicValue(valueOfN){
+    let result = 0;
+    for(var index = 1; index <= valueOfN; index++){
+        result += 1/index;
+    }
+    return result;
+}
+
+const inputValidator = (event) => {
+    if(!Number(event.key)){
+        alert('Please enter a number');
+        event.preventDefault();
     }
 }
         
