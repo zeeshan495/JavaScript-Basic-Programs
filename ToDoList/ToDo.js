6c7066751df747bcc6a136e1b1bd3e50903c59f9
zeeshan495 <38341567+zeeshan495@users.noreply.github.com>
2020-12-21 10:20:14 +0530

    Updating To Do List code changes for code review

--- ToDoList/ToDo.js
+++ ToDoList/ToDo.js
@@ -1,5 +1,32 @@
-function addTask(){
-    var input = document.getElementById('givenInput');
-    document.getElementById('note').innerHTML = input.value;
+// JavaScript source code
 
+function addTask() {
+    var i;
+    let taskName = document.getElementById('taskName').value;
+    if (taskName) {
+        let myList = document.getElementById('myList');
+        myList.setAttribute("style", "list-style-type:none;");
+        myList.innerHTML += "<li class= 'listItem' style='font - size: 90px'> <input type='checkbox' class='selected'/>"
+            + taskName + "<button id = 'deleteButton' class='delete'>Delete</button> </li>";
+        document.getElementById('taskName').value = '';
+    }
+    let deleteTask = document.getElementsByClassName("delete");
+    let selectTask = document.getElementsByClassName("selected");
+    
+    for (i = 0; i < deleteTask.length; i++) {
+        deleteTask[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+        selectTask[i].onclick = function selectTask() {
+            let selected = document.getElementsByClassName("selected");
+            let listItem = document.getElementsByClassName("listItem");
+            for (i = 0; i < selected.length; i++) {
+                if (selected[i].checked) 
+                    listItem[i].style.textDecoration = "line-through";
+                else
+                    listItem[i].style.textDecoration = "";
+            }
+        };
+    }
 }
